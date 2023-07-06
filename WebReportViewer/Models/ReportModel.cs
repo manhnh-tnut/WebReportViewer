070cf1ae41a50d8af1aaa0a131f8e1a9abb35b80
manh.nguyen11 <manh.nguyen11@sotatek.com>
2023-07-06 08:58:26 +0700

    Add project files.

---
+++ WebReportViewer/Models/ReportModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebReportViewer.Models
+{
+    public class ReportModel
+    {
+        public string Material  { get; set; }
+        public string Spec { get; set; }
+        public string Unit { get; set; }
+        public int Qty { get; internal set; }
+        public string Invoice { get; set; }
+        public string Remark { get; set; }
+    }
+}
---
+++ WebReportViewer/Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using AspNetCore.Reporting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using WebReportViewer.Models;
+
+namespace WebReportViewer.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IHostEnvironment _hostEnvironment;
+
+        public IndexModel(ILogger<IndexModel> logger, IHostEnvironment hostEnvironment)
+        {
+            _logger = logger;
+            _hostEnvironment = hostEnvironment; // has ContentRootPath property
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+        }
+
+        public IActionResult OnGet()
+        {
+            string mineType = "";
+            int extension = 1;
+            string reportPath = $"{_hostEnvironment.ContentRootPath}\\Reports\\InputSlipReport.rdlc";
+            Dictionary<string, string> parameters = new();
+            parameters.Add("doc", System.DateTime.Now.ToString("yyyyMMddHHmmss"));
+            List<ReportModel> data = new() { new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 }, new ReportModel() { Qty = 1 } };
+            LocalReport report = new(reportPath);
+            report.AddDataSource("DataSet1", data);
+            ReportResult result = report.Execute(RenderType.Pdf, extension, parameters, mineType);
+            return File(result.MainStream, "application/pdf");
+        }
+    }
+}
